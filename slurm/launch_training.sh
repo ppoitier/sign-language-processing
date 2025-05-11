5d35b96e33c2207c03535e6bead9652bd10f39a0
ppoitier <hanofound@hormail.com>
2025-05-11 11:50:06 +0200

    Fix script path in slurm file

--- slurm/launch_training.sh
+++ slurm/launch_training.sh
@@ -33,5 +33,5 @@ config_file=${config_files[$SLURM_ARRAY_TASK_ID]}
 nvidia-smi
 echo "Job array ID: $SLURM_ARRAY_TASK_ID"
 echo "Job start at $(date)"
-python ../script/segmentation/train.py --config-path="$config_file"
+python ../scripts/segmentation/train.py --config-path="$config_file"
 echo "Job end at $(date)"
