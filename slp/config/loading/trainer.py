341ba627270b02907a06bcd7fabcce976094bb8f
ppoitier <hanofound@hormail.com>
2025-05-15 12:41:16 +0200

    Fix datasets paths in config files

--- slp/config/loading/trainer.py
+++ slp/config/loading/trainer.py
@@ -11,13 +11,16 @@ def load_segmentation_lightning_module(config: SegmentationTaskConfig, training_
     target_codec_name = config.target_codec.name
     target_codec = load_segments_codec(config.target_codec)
     backbone = load_segmentation_backbone(config.backbone)
-
     criterion_weights = None
     if config.training.criterion.use_weights:
         assert training_dataset is not None, "Training dataset is required to use class weights."
         criterion_weights = torch.from_numpy(training_dataset.get_label_weights(config.target_codec.name)).float()
         print("Use criterion weights:", criterion_weights.tolist())
     criterion = load_segmentation_criterion(config.training.criterion, criterion_weights=criterion_weights)
+    checkpoint_path = config.backbone.checkpoint_path
+    if checkpoint_path is not None:
+        print("Loading checkpoint:", checkpoint_path)
+        return SegmentationTrainer.load_from_checkpoint(checkpoint_path, backbone=backbone, criterion=criterion)
     return SegmentationTrainer(
         codec=(target_codec_name, target_codec),
         backbone=backbone,
--- slp/config/templates/module.py
+++ slp/config/templates/module.py
@@ -4,3 +4,4 @@ from pydantic import BaseModel
 class ModuleConfig(BaseModel):
     module_name: str
     module_kwargs: dict = {}
+    checkpoint_path: str | None = None
