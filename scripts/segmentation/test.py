db8b764af27a307ca0c337a12ce9527dc5dc517a
ppoitier <hanofound@hormail.com>
2025-05-21 10:08:27 +0200

    Update config files

--- scripts/segmentation/test.py
+++ scripts/segmentation/test.py
@@ -1,4 +1,5 @@
 import click
+import os
 
 from slp.config.load_config import load_segmentation_task_config
 from slp.config.loading.tasks import load_segmentation_task
@@ -9,20 +10,21 @@ from slp.trainers.utils import run_training, run_testing
 @click.option(
     "-c", "--config-path", required=True, type=click.Path(exists=True, dir_okay=False)
 )
-@click.option(
-    "-p", "--checkpoint-path", required=True, type=click.Path(exists=True, dir_okay=False)
-)
-def launch_segmentation_testing(config_path, checkpoint_path):
+def launch_segmentation_testing(config_path):
     config = load_segmentation_task_config(config_path)
     task_id, data_sets, data_loaders, module = load_segmentation_task(config)
     exp_name = f"{task_id}_{str(int(config.task_datetime.timestamp() * 1000))}"
     log_dir = f"{config.output_dir}/logs/{exp_name}"
+    if config.backbone.checkpoint_path is None:
+        raise ValueError("Checkpoint path is required for testing.")
+    if not os.path.isfile(config.backbone.checkpoint_path):
+        raise FileNotFoundError("Checkpoint file not found.")
     print("Logs directory:", log_dir)
     run_testing(
         module,
         data_loaders,
         log_dir=log_dir,
-        checkpoint_path=checkpoint_path,
+        checkpoint_path=config.backbone.checkpoint_path,
         debug=config.training.debug,
     )
 
