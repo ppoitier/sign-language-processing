b31200c94156161d94dc695575e5440a5451b5f3
ppoitier <hanofound@hormail.com>
2025-10-06 15:02:13 +0200

    Change optimizer and increse patience of the early stopping

--- slp/tasks/isolated_recognition/trainer.py
+++ slp/tasks/isolated_recognition/trainer.py
@@ -74,7 +74,7 @@ class IsolatedRecognitionTrainer(TrainerBase):
         # todo
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate, eps=1e-3)
+        optimizer = optim.AdamW(self.parameters(), lr=self.learning_rate, eps=1e-3)
         return optimizer
         # scheduler = create_warmup_plateau_cosine_scheduler(
         #     optimizer,
