9ad421a5c3571e6ce73e6e8f78260ef0cf574bb8
ppoitier <pierre.poitier@unamur.be>
2025-11-24 13:37:38 +0100

    fix config paths in slurm config

--- slurm/recognition/launch_training.sh
+++ slurm/recognition/launch_training.sh
@@ -27,11 +27,11 @@ module load FFmpeg/7.0.2-GCCcore-13.3.0
 source /gpfs/home/acad/unamur-fac_info/ppoitier/envs/slp/bin/activate
 
 config_files=(
-  "../../configs/islr/lsfb/pose2rgb/r18/lsfb500_r18_base.yaml"
-  "../../configs/islr/lsfb/pose2rgb/r50/lsfb500_r50_base.yaml"
-  "../../configs/islr/lsfb/pose2rgb/r152/lsfb500_r152_base.yaml"
-  "../../configs/islr/lsfb/pose2rgb/pose-vit/lsfb500_posevit_jerome.yaml"
-  "../../configs/islr/lsfb/pose2rgb/spoter/lsfb500_spoter_base.yaml"
+  "../../configs/islr/lsfb/pose2rgb_500/r18/lsfb500_r18_base.yaml"
+  "../../configs/islr/lsfb/pose2rgb_500/r50/lsfb500_r50_base.yaml"
+  "../../configs/islr/lsfb/pose2rgb_500/r152/lsfb500_r152_base.yaml"
+  "../../configs/islr/lsfb/pose2rgb_500/pose-vit/lsfb500_posevit_jerome.yaml"
+  "../../configs/islr/lsfb/pose2rgb_500/spoter/lsfb500_spoter_base.yaml"
 )
 
 config_file=${config_files[$SLURM_ARRAY_TASK_ID]}
