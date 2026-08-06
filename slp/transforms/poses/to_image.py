3be1b4a37bf6109263cff8136c5fffc56e931716
ppoitier <pierre.poitier@unamur.be>
2026-08-06 11:40:41 +0200

    Add ToImageDropZ transform

--- slp/transforms/poses/to_image.py
+++ slp/transforms/poses/to_image.py
@@ -6,6 +6,7 @@ from sign_language_tools.pose.transforms import (
     ToRGBImage,
     TemporalCrop,
     Resample,
+    DropCoordinates,
 )
 
 from slp.core.registry import POSE_TRANSFORM_REGISTRY
@@ -19,6 +20,25 @@ class ToImage:
                 Concatenate(["upper_pose", "left_hand", "right_hand"]),
                 # NormalizeByReferenceEdge(ref_edge=(11, 12)),
                 # CenterOnLandmarks((11, 12)),
+                TemporalCrop(64),
+                Resample(64, 'nearest'),
+                ToRGBImage(),
+            ]
+        )
+
+    def __call__(self, poses):
+        return self.transform(poses)
+
+
+@POSE_TRANSFORM_REGISTRY.register("to-image-drop-z")
+class ToImageDropZ:
+    def __init__(self):
+        self.transform = Compose(
+            [
+                Concatenate(["upper_pose", "left_hand", "right_hand"]),
+                # NormalizeByReferenceEdge(ref_edge=(11, 12)),
+                # CenterOnLandmarks((11, 12)),
+                DropCoordinates('z'),
                 TemporalCrop(64),
                 Resample(64, 'nearest'),
                 ToRGBImage(),
